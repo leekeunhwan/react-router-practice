eceaeb724d1228613465e95ffa8c75f7c4a299d9
leekeunhwan <dlrmsghks7@gmail.com>
2018-06-19 15:16:55 +0900

    React-Router Tutorial-Basic

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <div>
+            <Link to="/page1">Page 1</Link>
+            <Link to="/page2">Page 2</Link>
+            <Link to="/page3">Page 3</Link>
+          </div>
+          <Switch>
+            <Route path="/page1" component={Page1} />
+            <Route path="/page2" component={Page2} />
+            <Route path="/page3" component={Page3} />
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+const Home = () => <div>Home</div>;
+
+const Page1 = () => <div>Page1</div>;
+
+const Page2 = () => <div>Page2</div>;
+
+const Page3 = () => <div>Page3</div>;
+
+export default App;
